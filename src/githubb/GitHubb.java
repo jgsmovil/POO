bfece1f351c79e2d17a73c897450f8ac89c1393c
Juana Garcia Seañez <jjggsscia@gmail.com>
2021-04-19 11:25:34 -0600

    Programa despliega saludos, primer ejercicio

---
+++ src/githubb/GitHubb.java
@@ -0,0 +1,19 @@
+/*
+ Ejemplos para mostrar en Git Hub
+ */
+package githubb;
+
+/**
+ *
+ * @author jjggs_000
+ */
+public class GitHubb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es:  Juana Garcia Seañez");
+    }
+    
+}
